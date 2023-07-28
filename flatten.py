5f9b3a1d36e704bdd9e53881bd7df86e678ab208
Ryan Lee <ryantlee9@gmail.com>
2023-07-27 20:20:35 -0700

    Set spindle speed and rapid to zero at start

--- flatten.py
+++ flatten.py
@@ -5,6 +5,7 @@ SAFE_DISTANCE = 0.1
 
 @click.command()
 @click.option("--feed-rate", default=250, help="Feed rate in IPM")
+@click.option("--spindle-speed", default=12000, help="Spindle speed in RPM")
 @click.option(
     "--passes", default=1, help="Number of passes at a specific Z coordinate to take"
 )
@@ -22,9 +23,9 @@ SAFE_DISTANCE = 0.1
     prompt="Workpiece length",
     help="Length (Y axis) of the workpiece to flatten",
 )
-def gen_gcode(width, length, stepover, depth, passes, feed_rate):
+def gen_gcode(width, length, stepover, depth, passes, feed_rate, spindle_speed):
     """Generate g code for flattening slabs."""
-    gcode = f"\nG90\nF{feed_rate}\nG1"
+    gcode = f"\nG90\nF{feed_rate}\nG0 Z{SAFE_DISTANCE}\nS{spindle_speed} M3"
     num_x_steps = math.ceil(width / stepover / 2)
     for i in range(passes):
         gcode += f"\n(Pass number {i + 1})\n"
