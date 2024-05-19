4ff602a3d0280520d55c13e2cd247b7f120159db
Ryan Lee <ryantlee9@gmail.com>
2024-05-18 20:35:43 -0700

    Add option to skip a direction
    CNC will flatten in one direction, reverse without shifting
    X position, then shift before proceeding

--- flatten.py
+++ flatten.py
@@ -6,6 +6,7 @@ SAFE_DISTANCE = 0.1
 @click.command()
 @click.option("--feed-rate", default=250, help="Feed rate in IPM")
 @click.option("--spindle-speed", default=12000, help="Spindle speed in RPM")
+@click.option("--skip-direction", default=None, help="Direction to skip (forward, backwards)")
 @click.option(
     "--passes", default=1, help="Number of passes at a specific Z coordinate to take"
 )
@@ -23,16 +24,21 @@ SAFE_DISTANCE = 0.1
     prompt="Workpiece length",
     help="Length (Y axis) of the workpiece to flatten",
 )
-def gen_gcode(width, length, stepover, depth, passes, feed_rate, spindle_speed):
+def gen_gcode(width, length, stepover, depth, passes, feed_rate, spindle_speed, skip_direction):
     """Generate g code for flattening slabs."""
     gcode = f"\nG90\nF{feed_rate}\nG0 Z{SAFE_DISTANCE}\nG0 X0 Y0\nS{spindle_speed} M3"
-    num_x_steps = math.ceil(width / max(stepover, -stepover) / 2)
+    num_x_steps = width / max(stepover, -stepover) / 2
+    if skip_direction:
+        num_x_steps *= 2
+    num_x_steps = math.ceil(num_x_steps)
     for i in range(passes):
         gcode += f"\n(Pass number {i + 1})\n"
         # move spindle down
         gcode += f"\nG0 X0 Y0\nG1 Z-{(i + 1) * depth}\n"
         for x_step in range(num_x_steps):
-            gcode += f"\nX{stepover * x_step * 2}\nY{length}\nX{stepover * (x_step * 2 + 1)}\nY0\n"
+            if skip_direction:
+                x_step /= 2
+            gcode += f"\nX{stepover * x_step * 2}\nY{length}\nX{stepover * (x_step * 2 + (0 if skip_direction == 'backwards' else 1))}\nY0\n"
         # move spindle up safe distance
         gcode += f"\nZ{SAFE_DISTANCE - i * depth}"
     gcode += "\nM30"
